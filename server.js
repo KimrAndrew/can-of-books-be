02c1b66a930be158fa1e990adf3df179467b66b3
KimrAndrew <andrewrk1997@gmail.com>
2021-11-08 14:47:26 -0800

    created mongo connection

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function() {
+  console.log('Mongoose is connected!');
+});
+
+
 const app = express();
 app.use(cors());
 
